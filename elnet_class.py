1e3d87ca523df76a59d47ed05bf3cbb52ac68c05
epockonechny <pokonechnyy.ep@phystech.edu>
2018-03-23 18:10:21 +0300

    changes

--- elnet_class.py
+++ elnet_class.py
@@ -7,10 +7,10 @@ from sklearn.linear_model import ElasticNet
 class ElasticNetPredictor:
     def __init__(self, alpha=1.0, l1_ratio=0.5, fit_intercept=True, normalize=False, precompute=False,
                  max_iter=1000, copy_X=True, tol=1e-4, warm_start=False, positive=False, random_state=None,
-                 selection='cyclic', words_data=None, documents_data=None):
+                 selection='cyclic'):
         self.feature_to_predict = None
-        self.words_data = words_data
-        self.documents_data = documents_data
+        self.words_data = None
+        self.documents_data = None
         self.model = ElasticNet(alpha, l1_ratio, fit_intercept, normalize, precompute, max_iter, copy_X, tol,
                                 warm_start, positive, random_state, selection)
         self.label = None
@@ -32,6 +32,7 @@ class ElasticNetPredictor:
         return list(set(df.columns).difference(ans))
 
     def fit(self, train_feature, train_labels):
+        print('Start fitting')
         self.model.fit(train_feature, train_labels)
 
     def process_data(self, documents_data=None, words_data=None, previous_periods=2, previous_words=1,
@@ -120,3 +121,40 @@ class ElasticNetPredictor:
         data = self.friendly_prediction(number_of_periods)
         data = data.apply(lambda row: row / np.sum(row), axis=1)
         return data
+
+    
+class Formater:
+    def __init__(self, model=ElasticNetPredictor(), row_per_day=12, previous_days=4, previous_words=1,
+                 words_to_perform=4000, collinear_threshold=0.56):
+        self.model = model
+        self.row_per_day = row_per_day
+        self.previous_days = previous_days
+        self.previous_words = previous_words
+        self.words_to_perform = words_to_perform
+        self.collinear_threshold = collinear_threshold
+
+    def fit(self, counted_data, word_data):
+        self.model.process_data(counted_data, word_data, previous_periods=int(self.row_per_day*self.previous_days),
+                                previous_words=int(self.row_per_day*self.previous_words),
+                                word_sample_size=self.words_to_perform, collinear_threshhold=self.collinear_threshold)
+        self.last_day_count = counted_data.values[-self.row_per_day:, :].sum(axis=0)
+
+    def predict(self, days_to_predict=7):
+        num_titles = self.model.number_of_titles
+        predicted_values = self.model.friendly_prediction(days_to_predict * self.row_per_day)
+        predicted_probas = self.model.compute_proba(days_to_predict * self.row_per_day)
+        values = predicted_values.values
+        values = values.reshape((self.row_per_day, num_titles, days_to_predict))
+        summed_val = values.sum(axis=0)
+
+        probas = predicted_probas.values
+        probas = probas.reshape((self.row_per_day, num_titles, days_to_predict))
+        daily_probas = probas.mean(axis=0)
+        
+        summed_val = np.hstack((self.last_day_count.reshape((-1, 1)), summed_val))
+        daily_probas = np.hstack((self.last_day_count.reshape((-1, 1)) / self.last_day_count.sum(), daily_probas))
+        
+        daily_forecast = []
+        for i in np.arange(days_to_predict):
+            daily_forecast.append([{'topic':predicted_values.columns[j], 'documents': summed_val[j, i + 1], 'diff_documents': summed_val[j, i + 1] - summed_val[j, i], 'percentage': daily_probas[j, i + 1], 'diff_percentage' : daily_probas[j, i + 1] - daily_probas[j, i]} for j in range(len(predicted_values.columns))])
+        return daily_forecast
